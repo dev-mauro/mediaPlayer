0039c9c8ffc133d27f43d33bcc6411be869934e1
Mauro Silva <silvac.mauro@gmail.com>
2021-07-19 19:05:00 -0400

    Implementacion AutoPause plugin y comentarios. Cambios a la estructura HTML y CSS

--- assets/index.js
+++ assets/index.js
@@ -1,12 +1,13 @@
 import MediaPlayer from './MediaPlayer.js';
 import AutoPlay from './plugins/AutoPlay.js';
 import Loop from './plugins/Loop.js';
+import AutoPause from './plugins/AutoPause.js';
 
 const video = document.querySelector('video');
 const playButton = document.getElementById('play-button');
 const muteButton = document.getElementById('mute-button');
 
-const player = new MediaPlayer({ el: video, plugins: [new AutoPlay(), new Loop()] });
+const player = new MediaPlayer({ el: video, plugins: [new AutoPlay(), new Loop(), new AutoPause()] });
 
 playButton.onclick = () => player.togglePlay();
 muteButton.onclick = () => player.toggleMute();
