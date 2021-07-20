b69136ee0e8c94d421b59ef75225aa7b9d324fa1
Mauro Silva <silvac.mauro@gmail.com>
2021-07-19 20:06:42 -0400

    Mejora al AutoPause Plugin

--- assets/plugins/AutoPause.js
+++ assets/plugins/AutoPause.js
@@ -1,20 +1,36 @@
 class AutoPause  {
+    constructor() {
+        this.threshold = 0.25;
+        this.handleIntersection = this.handleIntersection.bind(this);
+        this.handleVisibilityChange = this.handleVisibilityChange.bind(this);
+    }
+
     run(player) {
+        this.player = player;
+
         const observer = new IntersectionObserver(this.handleIntersection, {
-            threshold: 0.25,
+            threshold: this.threshold,
         });
         observer.observe(player.media);
 
-        document.addEventListener("visibilitychange", () => {
-            player.togglePlay();
-        });
+        document.addEventListener("visibilitychange", this.handleVisibilityChange);
     }
 
     handleIntersection(entries) {
         let entry = entries[0];
-        (entry.intersectionRatio < 0.25)
+        const isVisible = entry.intersectionRatio < this.threshold;
+        (isVisible)
         ? entry.target.pause()
         : entry.target.play();
+    }
+
+    handleVisibilityChange() {
+        const isVisible = document.visibilityState === 'visible';
+        if(isVisible) {
+            this.player.media.play();
+        } else {
+            this.player.media.pause();
+        }
     }
 }
 
