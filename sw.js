5758c0c2f367a2e24691fbc8801ac6243ce272e3
Mauro Silva <silvac.mauro@gmail.com>
2021-07-20 17:24:07 -0400

    Implementacion Service Workers

---
+++ sw.js
@@ -0,0 +1,45 @@
+const VERSION = "v1";
+
+self.addEventListener('install', event => {
+    event.waitUntil(precache());
+});
+
+self.addEventListener('fetch', event => {
+    const request = event.request;
+    if (request.method =! "GET"){
+        return;
+    }
+
+    // buscar en cache
+    event.respondWith(cacheResponse(request));
+
+    // actualizar el cache
+    event.waitUntil(updateCache(request));
+})
+
+async function precache() {
+    const cache = await caches.open(VERSION);
+    return cache.addAll([
+        '/',
+        '/index.html',
+        '/assets/index.css',
+        '/assets/index.js',
+        '/assets/MediaPlayer.js',
+        '/assets/plugins/AutoPause.js',
+        '/assets/plugins/AutoPlay.js',
+        '/assets/plugins/Loop.js',
+        '/assets/monkey_video.mp4',
+    ]);
+}
+
+async function cacheResponse(request) {
+    const cache = await caches.open(VERSION);
+    const response = await cache.match(request);
+    return response || fetch(request);
+}
+
+async function updateCache(request) {
+    const cache = await caches.open(VERSION);
+    const response = await fetch(request);
+    return cache.put(request, response);
+}
